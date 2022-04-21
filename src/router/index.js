545654dffa5ba6e839d8cace171382dfc1964cb1
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-21 12:19:56 -0700

    adds SongsIndex page/route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SongsIndex from '../views/SongsIndex.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/songs',
+    name: 'songs',
+    component: SongsIndex
   },
   {
     path: '/about',
---
+++ src/views/SongsIndex.vue
@@ -0,0 +1,36 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Song index page",
+      songs: []
+    };
+  },
+  created: function () {
+    this.songsIndex()
+  },
+  methods: {
+    songsIndex: function () {
+      console.log('all songs')
+      axios.get(`/songs`).then(response => {
+        console.log(response.data)
+        this.songs = response.data
+      })
+    }
+  },
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div v-for="song in songs" v-bind:key="song.id">
+      <p>{{ song.title }}</p>
+    </div>
+
+  </div>
+</template>
+
+<style>
+</style>
