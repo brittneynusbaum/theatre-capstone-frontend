1ca0c5518c88f90004da839aa01ad9fa8c87b1bd
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-27 23:38:51 -0700

    adds search params and input for show

--- src/views/SongsIndex.vue
+++ src/views/SongsIndex.vue
@@ -5,27 +5,31 @@ export default {
     return {
       message: "Song index page",
       songs: [],
-      searchSongsParams: {}
+      searchSongwriterParams: {},
+      searchShowParams: {}
     };
   },
   created: function () {
     // this.songsIndex()
   },
   methods: {
-    songsIndex: function () {
+    songwriterSongsIndex: function () {
       console.log('all songs')
       console.log(this.songwriter_id)
-      axios.get(`/songs?composer=${this.searchSongsParams}`).then(response => {
+      axios.get(`/songs?composer=${this.searchSongwriterParams}&title=${this.searchShowParams}`).then(response => {
         console.log(response.data)
         this.songs = response.data
       })
     },
+    showSongsIndex: function () {
+      console.log('all songs based on show')
+    }
     // searchSongs: function () {
     //   console.log('searching songs')
     //   return this.songs.filter(song => {
     //     var lowerTitle = song.title.toLowerCase();
-    //     var lowerParams = this.searchSongsParams.toLowerCase();
-    //     return lowerTitle.includes(this.searchSongsParams);
+    //     var lowerParams = this.searchSongwriterParams.toLowerCase();
+    //     return lowerTitle.includes(this.searchSongwriterParams);
     //   })
     // }
   },
@@ -37,10 +41,10 @@ export default {
     <h1>{{ message }}</h1>
     <div v-for="song in songs" v-bind:key="song.id">
       <p>{{ song.title }}</p>
-      <p>{{ song.songwriter_id }}</p>
     </div>
-    <p>Songwriter: <input v-model="searchSongsParams"></p>
-    <button v-on:click="songsIndex()">Search songs</button>
+    <p>Songwriter: <input v-model="searchSongwriterParams"></p>
+    <p>Show: <input v-model="searchShowParams"></p>
+    <button v-on:click="songwriterSongsIndex()">Search songs</button>
 
   </div>
 </template>
