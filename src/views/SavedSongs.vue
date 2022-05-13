eadd9166e2f7c337014380ae5554619038f6495f
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-05-13 09:39:40 -0700

    preservation commit before styling

--- src/views/SavedSongs.vue
+++ src/views/SavedSongs.vue
@@ -4,18 +4,27 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      auditionSongs: []
+      auditionSongs: [],
+      notes: []
     };
   },
   created: function () {
-    this.auditionSongsIndex()
+    this.auditionSongsIndex(),
+      this.notesIndex()
   },
   methods: {
     auditionSongsIndex: function () {
       console.log('all saved songs')
       axios.get(`/saved_songs`).then(response => {
-        console.log(response.data)
+        console.log(response.data);
         this.auditionSongs = response.data
+      })
+    },
+    notesIndex: function () {
+      console.log('index notes')
+      axios.get(`/notes`).then(response => {
+        console.log(response.data);
+        this.notes = response.data
       })
     }
   },
@@ -27,7 +36,15 @@ export default {
     <h1>Saved songs page!</h1>
     <!-- {{ auditionSongs }} -->
     <div v-for="auditionSong in auditionSongs" v-bind:key="auditionSong.id">
+      <!-- <p>{{ notes }}</p> -->
+      <!-- <p>{{ notes[1][0].description }}</p> -->
       <p>{{ auditionSong.title }}</p>
+      <!-- <p>{{ auditionSong.id }}</p>
+      <p>{{ notes }}</p> -->
+      <!-- <div v-for="note in notes" v-bind:key="note.id">
+        <p v-if="auditionSong.id == note[0].saved_song_id">Notes: {{ note[0].description }}</p>
+        <p>Add note:<input type="text"></p>
+      </div> -->
     </div>
   </div>
 </template>
